da5159a3c30e87c175e207469d89e339247b2953
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2025-01-30 15:04:11 -0600

    style: :lipstick: Updates colors based on tailwind.config scheme

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,22 +1,50 @@
 import { IconService } from '@/app/core/services/icon.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { CurrencySelectorComponent } from '../../../modules/clients/features/currency-selector/currency-selector.component';
+import { DarkModeSwitcherComponent } from '../dark-mode-switcher/dark-mode-switcher.component';
+import { CollectionService } from '@/app/core/services/collection.service';
+import { CollectionsNamePipe } from '@/app/core/pipes/collections-name.pipe';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, CommonModule, CurrencySelectorComponent],
+  imports: [
+    MatIcon,
+    CommonModule,
+    CurrencySelectorComponent,
+    DarkModeSwitcherComponent,
+    CollectionsNamePipe,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   _isSidebarOpen: boolean = false;
   _isUserMenuOpen: boolean = false;
+  data: any[] = [];
 
-  constructor(private _iconService: IconService) {}
+  constructor(
+    private _iconService: IconService,
+    private _collectionService: CollectionService
+  ) {}
 
+  ngOnInit(): void {
+    // Llamamos al servicio para obtener los datos
+    this._collectionService.getData().subscribe(
+      (response) => {
+        this.data = response.urlset.url.sort((first: any, second: any) => {
+          const textA = first.loc._text.toLowerCase();
+          const textB = second.loc._text.toLowerCase();
+          return textA < textB ? -1 : textA > textB ? 1 : 0;
+        }); // Asignamos los datos obtenidos a la variable 'data'
+      },
+      (error) => {
+        console.error('Error al obtener los datos:', error);
+      }
+    );
+  }
   toggleSidebar() {
     this._isSidebarOpen = !this._isSidebarOpen;
   }
